059527810153a025ee3e4f4e47c58199e2a82e5c
Wen <1534364995@qq.com>
2019-09-23 15:59:17 +0800

    init repo

---
+++ src/main/java/life/why/commnuity/CommnuityApplication.java
@@ -0,0 +1,13 @@
+package life.why.commnuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommnuityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommnuityApplication.class, args);
+    }
+
+}
